4f2fa2182719edc53141e13dd8de4dd2afaaf3cf
Hasya Patel <Studyfor1505@gmail.com>
2026-04-21 15:30:44 +0530

    Add max_sum_subarray function for sliding window
    Implement sliding window algorithm to find maximum sum of subarray of size k.

---
+++ DAY1/1_7.py
@@ -0,0 +1,18 @@
+# Q7. Python – Sliding Window (DSA Concept)
+
+# Find the maximum sum subarray of size k
+
+# 👉 Example:
+
+# arr = [2,1,5,1,3,2], k = 3
+# Output: 9
+
+def max_sum_subarray(arr, k):
+    window_sum = sum(arr[:k])
+    max_sum = window_sum
+    
+    for i in range(k, len(arr)):
+        window_sum += arr[i] - arr[i-k]
+        max_sum = max(max_sum, window_sum)
+    
+    return max_sum
